156be891b1ddd667d92a55823127d8f4a53623da
FingerPrintGames <kevinjordan199449@gmail.com>
2022-01-30 11:47:36 -0500

    Set up Soda Can and some geometry

---
+++ Soda Poppin/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] float thrustForce = 1000f;
+    [SerializeField] float rotationForce = 500f;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        bool boostInput = Input.GetKey(KeyCode.Space);
+
+        if (boostInput)
+        {
+            Vector3 force = (Vector3.up * thrustForce * Time.deltaTime);
+            rb.AddRelativeForce(force);
+        }
+    }
+    
+    void ProcessRotation()
+    {
+        bool leftInput = Input.GetKey(KeyCode.A);
+        bool rightInput = Input.GetKey(KeyCode.D);
+
+        if (leftInput && !rightInput)
+        {
+            ApplyRotation(rotationForce);
+        }
+        if (rightInput && !leftInput)
+        {
+            ApplyRotation(-rotationForce);
+        }
+    }
+
+    void ApplyRotation(float rotationAmountPerFrame)
+    {
+        rb.freezeRotation = true;
+        Vector3 force = Vector3.forward * rotationAmountPerFrame * Time.deltaTime;
+        transform.Rotate(force);
+        rb.freezeRotation = false;
+    }
+}
