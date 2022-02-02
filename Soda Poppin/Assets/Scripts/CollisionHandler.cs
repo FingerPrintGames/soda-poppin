57f2c8ff2a8e670ce97de1ab1a54729d9debe5b6
FingerPrintGames <kevinjordan199449@gmail.com>
2022-02-02 15:08:54 -0500

    Added 2nd level and transitions between levels

--- Soda Poppin/Assets/Scripts/CollisionHandler.cs
+++ Soda Poppin/Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,20 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadLevelDelay = 2f;
+    
+    Movement movement;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        movement = GetComponent<Movement>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -11,11 +24,13 @@ public class CollisionHandler : MonoBehaviour
                 break;
 
             case "Finish":
-                Debug.Log("Level Complete!");
+                DisableControls();
+                StartCoroutine(GoToNextLevel());
                 break;
 
             default:
-                Debug.Log("You crashed!");
+                DisableControls();
+                StartCoroutine(RestartLevel());
                 break;
         }
     }
@@ -26,6 +41,34 @@ public class CollisionHandler : MonoBehaviour
         {
             Debug.Log("Fuel collected");
             Destroy(other.gameObject);
+        }
+    }
+
+    void DisableControls()
+    {
+        movement.enabled = false;
+        audioSource.enabled = false;
+    }
+    
+    IEnumerator RestartLevel()
+    {
+        yield return new WaitForSeconds(loadLevelDelay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator GoToNextLevel()
+    {
+        yield return new WaitForSeconds(loadLevelDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int totalScenes = SceneManager.sceneCountInBuildSettings;
+
+        if (currentSceneIndex + 1 < totalScenes)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
